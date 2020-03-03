80626ae1aa748381285a1691285132af5cf40381
Amos Wenger <amoswenger@gmail.com>
2020-03-03 20:52:17 +0100

    download nasm from nasm.us again

--- vendor-download.sh
+++ vendor-download.sh
@@ -3,8 +3,7 @@ source vendor-env.sh
 
 mkdir -p $GOFF_PREFIX/src
 pushd $GOFF_PREFIX/src
-# curl -L https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.bz2 | tar -xj
-curl -L https://repo.or.cz/nasm.git/snapshot/74246c499ea4313fb8837977dc0c135fc50567c0.tar.gz | tar -xz
+curl -L https://www.nasm.us/pub/nasm/snapshots/latest/nasm-2.15rc0-20191023.tar.gz | tar -xz
 curl -L https://code.videolan.org/videolan/x264/-/archive/1771b556ee45207f8711744ccbd5d42a3949b14c/x264-1771b556ee45207f8711744ccbd5d42a3949b14c.tar.bz2 | tar -xj
 curl -L https://ffmpeg.org/releases/ffmpeg-4.2.2.tar.bz2 | tar -xj
 popd
